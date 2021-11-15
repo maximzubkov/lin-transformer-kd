cb76b64f549282adfbbb4cd05029c85d86969f68
maximzubkov <zubkov.md@phystech.edu>
2021-11-15 19:13:49 +0100

    Fix duplicated code

--- tests/test_lin_attention.py
+++ tests/test_lin_attention.py
@@ -4,8 +4,7 @@ from modules.linear_attention import LinearAttention, CausalLinearAttention
 
 
 @torch.no_grad()
-def test_lin_attn():
-    attn = LinearAttention()
+def _run_attention(attn):
     attn.eval()
 
     seq_len = 16
@@ -23,20 +22,12 @@ def test_lin_attn():
 
 
 @torch.no_grad()
+def test_lin_attn():
+    attn = LinearAttention()
+    _run_attention(attn)
+
+
+@torch.no_grad()
 def test_causal_lin_attn():
     attn = CausalLinearAttention()
-    attn.eval()
-
-    seq_len = 16
-    batch_size = 32
-    embed_dim = 24
-    num_heads = 8
-
-    q = torch.rand(batch_size, seq_len, num_heads, embed_dim)
-    v = torch.rand(batch_size, seq_len, num_heads, embed_dim)
-    k = torch.rand(batch_size, seq_len, num_heads, embed_dim)
-
-    v_1, _ = attn.forward(q, k, v, output_attention=True)
-    v_2, _ = attn.forward(q, k, v, output_attention=False)
-    assert torch.allclose(v_1, v_2, atol=1e-4)
-
+    _run_attention(attn)
