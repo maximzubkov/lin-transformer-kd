626aaeae8383a9868b066cde0fdd67f59d4e2f18
maximzubkov <zubkov.md@phystech.edu>
2021-11-17 21:36:36 +0100

    Upd test for onnx

--- tests/test_onnx.py
+++ tests/test_onnx.py
@@ -7,23 +7,22 @@ import torch
 
 from modules.linear_attention import LinearAttention, CausalLinearAttention
 
+seq_len = 64
+batch_size = 8
+embed_dim = 24
+num_heads = 2
+
+q = torch.rand(batch_size, seq_len, num_heads, embed_dim)
+v = torch.rand(batch_size, seq_len, num_heads, embed_dim)
+k = torch.rand(batch_size, seq_len, num_heads, embed_dim)
+
 
 @torch.no_grad()
 def _convert_to_onnx_quadratic(attn: Union[LinearAttention, CausalLinearAttention], model_name: str):
     attn.eval()
 
-    seq_len = 16
-    batch_size = 32
-    embed_dim = 24
-    num_heads = 8
-
-    q = torch.rand(batch_size, seq_len, num_heads, embed_dim)
-    v = torch.rand(batch_size, seq_len, num_heads, embed_dim)
-    k = torch.rand(batch_size, seq_len, num_heads, embed_dim)
-
     v_orig, attn_orig = attn.forward(q, k, v, output_attention=True)
 
-    # Export the model
     torch.onnx.export(
         attn,
         (q, k, v, True),
@@ -33,6 +32,13 @@ def _convert_to_onnx_quadratic(attn: Union[LinearAttention, CausalLinearAttentio
         do_constant_folding=True,
         input_names=['q', 'k', 'v', 'output_attention'],
         output_names=['values', 'attention'],
+        dynamic_axes={
+            "q": {0: "batch_size", 1: "sequence"},
+            "k": {0: "batch_size", 1: "sequence"},
+            "v": {0: "batch_size", 1: "sequence"},
+            "values": {0: "batch_size", 1: "sequence"},
+            "attention": {0: "batch_size", 2: "sequence", 3: "sequence"}
+        }
     )
     model = onnx.load(model_name)
     onnx.checker.check_model(model)
@@ -47,22 +53,13 @@ def _convert_to_onnx_quadratic(attn: Union[LinearAttention, CausalLinearAttentio
             "v": v.cpu().numpy(),
         },
     )
-    assert np.all(np.isclose(v_onnx, v_orig.cpu().numpy(), atol=1e-3))
-    assert np.all(np.isclose(attn_onnx, attn_orig.cpu().numpy(), atol=1e-3))
+    assert np.all(np.isclose(v_onnx, v_orig.cpu().numpy(), atol=1e-5))
+    assert np.all(np.isclose(attn_onnx, attn_orig.cpu().numpy(), atol=1e-5))
 
 
 @torch.no_grad()
 def _convert_to_onnx_linear(attn: Union[LinearAttention, CausalLinearAttention], model_name: str):
     attn.eval()
-
-    seq_len = 16
-    batch_size = 32
-    embed_dim = 24
-    num_heads = 8
-
-    q = torch.rand(batch_size, seq_len, num_heads, embed_dim)
-    v = torch.rand(batch_size, seq_len, num_heads, embed_dim)
-    k = torch.rand(batch_size, seq_len, num_heads, embed_dim)
 
     v_orig, _ = attn.forward(q, k, v, output_attention=False)
 
@@ -76,6 +73,12 @@ def _convert_to_onnx_linear(attn: Union[LinearAttention, CausalLinearAttention],
         do_constant_folding=True,
         input_names=['q', 'k', 'v', 'output_attention'],
         output_names=['values'],
+        dynamic_axes={
+            "q": {0: "batch_size", 1: "sequence"},
+            "k": {0: "batch_size", 1: "sequence"},
+            "v": {0: "batch_size", 1: "sequence"},
+            "values": {0: "batch_size", 1: "sequence"}
+        }
     )
     model = onnx.load(model_name)
     onnx.checker.check_model(model)
@@ -90,18 +93,28 @@ def _convert_to_onnx_linear(attn: Union[LinearAttention, CausalLinearAttention],
             "v": v.cpu().numpy(),
         },
     )
-    assert np.all(np.isclose(v_onnx, v_orig.cpu().numpy(), atol=1e-3))
+    assert np.all(np.isclose(v_onnx, v_orig.cpu().numpy(), atol=1e-5))
 
 
 @torch.no_grad()
-def test_lin_attn():
+def test_onnx_with_lin_attn():
     attn = LinearAttention()
-    _convert_to_onnx_quadratic(attn, model_name="linear-attn-naive.onnx")
     _convert_to_onnx_linear(attn, model_name="linear-attn.onnx")
 
 
 @torch.no_grad()
-def test_causal_lin_attn():
+def test_onnx_with_quad_attn():
+    attn = LinearAttention()
+    _convert_to_onnx_quadratic(attn, model_name="linear-attn-quad.onnx")
+
+
+@torch.no_grad()
+def test_onnx_with_causal_lin_attn():
     attn = CausalLinearAttention()
-    _convert_to_onnx_quadratic(attn, model_name="linear-attn-naive.onnx")
     _convert_to_onnx_linear(attn, model_name="causal-linear-attn.onnx")
+
+
+@torch.no_grad()
+def test_onnx_with_causal_quad_attn():
+    attn = CausalLinearAttention()
+    _convert_to_onnx_quadratic(attn, model_name="causal-linear-attn-quad.onnx")
